26e1f56f1f2a70bdc4fb91a8a153568783bd5f27
rafaeljgomezg <rgomezshares@hotmail.com>
2016-09-03 11:40:01 -0400

    Use of "let" and "const" variables

--- app.js
+++ app.js
@@ -1,10 +1,5 @@
 var container = document.getElementById('container');
-var prueba = document.getElementById('prueba');
-var test = true; // it is evaluated directly in the prueba.innerHTML expression
-var todo = {
-    id: 123,
-    name: 'Pick up drycleaning',
-    completed: true // it is evaluated directly in the container.innerHTML expression
-};
-container.innerHTML = "\n<div todo='" + todo.name + "' class=\"list-group-item}\">\n    <i class=\"" + (todo.completed ? "" : "hidden") + " text-success glyphicon glyphicon-ok\"></i>\n    <span class=\"name\">" + todo.name + "</span>\n</div>\n";
-prueba.innerHTML = " \n He named his car <i class ='" + (test ? "" : "hidden") + " text-success '>The lightning</i>, because it was very fast.\n";
+for (var x = 0; x <= 5; x++) {
+    var counter = x;
+}
+console.log(counter);
--- app.ts
+++ app.ts
@@ -1,20 +1,8 @@
 var container = document.getElementById('container');
-var prueba = document.getElementById('prueba');
-var test = true; // it is evaluated directly in the prueba.innerHTML expression
 
-var todo = {
-    id: 123,
-    name: 'Pick up drycleaning',
-    completed: true // it is evaluated directly in the container.innerHTML expression
-};
+for (var x = 0; x <= 5; x++) {
+    let counter = x;
+    //counter = 1;
+}
 
-container.innerHTML = `
-<div todo='${todo.name}' class="list-group-item}">
-    <i class="${todo.completed ? "" : "hidden"} text-success glyphicon glyphicon-ok"></i>
-    <span class="name">${todo.name}</span>
-</div>
-`
-
-prueba.innerHTML = ` 
- He named his car <i class ='${test ? "" : "hidden"} text-success '>The lightning</i>, because it was very fast.
-`
+console.log(counter);
