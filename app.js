f01e5515b1ec671099c0590d3015ab05aefa7cfe
rafaeljgomezg <rgomezshares@hotmail.com>
2016-09-03 15:21:36 -0400

    Chapter 02_09: Computed Properties

--- app.js
+++ app.js
@@ -1,71 +1,11 @@
-//Classic way to define a function with some values
-function add() {
-    var values = Array.prototype.splice.call(arguments, [1]), total = 0;
-    for (var _i = 0, values_1 = values; _i < values_1.length; _i++) {
-        var value = values_1[_i];
-        total += value;
-    }
-    return total;
+var osPrefix = 'os_';
+var support = (_a = {},
+    _a[osPrefix + 'Windows'] = isSupported('Windows'),
+    _a[osPrefix + 'iOS'] = isSupported('iOS'),
+    _a[osPrefix + 'Android'] = isSupported('Android'),
+    _a
+);
+function isSupported(os) {
+    return Math.random() >= 0.5;
 }
-//ES6 way
-function add1() {
-    var values = [];
-    for (var _i = 0; _i < arguments.length; _i++) {
-        values[_i - 0] = arguments[_i];
-    }
-    var total = 0;
-    for (var _a = 0, values_2 = values; _a < values_2.length; _a++) {
-        var value = values_2[_a];
-        total += value;
-    }
-    return total;
-}
-//Doing some calculation
-function calculate() {
-    var values = [];
-    for (var _i = 0; _i < arguments.length; _i++) {
-        values[_i - 0] = arguments[_i];
-    }
-    var total = 0;
-    for (var _a = 0, values_3 = values; _a < values_3.length; _a++) {
-        var value = values_3[_a];
-        total += value;
-    }
-    return total;
-}
-console.log(calculate(1, 2, 3, 4, 5, 6));
-//Doing calcalation with some options
-function calculate1(action) {
-    var values = [];
-    for (var _i = 1; _i < arguments.length; _i++) {
-        values[_i - 1] = arguments[_i];
-    }
-    var total = 0;
-    for (var _a = 0, values_4 = values; _a < values_4.length; _a++) {
-        var value = values_4[_a];
-        switch (action) {
-            case 'add':
-                total += value;
-                break;
-            case 'substract':
-                total -= value;
-                break;
-        }
-    }
-    return total;
-}
-console.log(calculate1('add', 1, 2, 3, 4, 5, 6));
-//Inserting an array inside another array
-var source = [3, 4, 5];
-var dest = [1, 2].concat(source, [6, 7, 8]);
-console.log(dest);
-//another example to concatenate two arrays (ES5 sintax)
-var list = [1, 2, 3];
-var toAdd = [4, 5, 6];
-Array.prototype.push.apply(list, toAdd);
-console.log(list);
-//Concatenate ES6 sintax
-var list = [1, 2, 3];
-var toAdd = [4, 5, 6];
-list.push.apply(list, toAdd);
-console.log(list);
+var _a;
--- app.ts
+++ app.ts
@@ -1,71 +1,11 @@
-//Classic way to define a function with some values
+const osPrefix = 'os_';
 
-function add(){
-    var values = Array.prototype.splice.call(arguments,[1]),
-    total=0;
-    for (var value of values){
-        total  += value;   
-    }
-    return total;
+var support = {
+    [osPrefix + 'Windows']: isSupported('Windows'),
+    [osPrefix + 'iOS']: isSupported('iOS'),
+    [osPrefix + 'Android']: isSupported('Android'),
 }
 
-
-//ES6 way
-
-function add1 (...values){
- var total=0;
-    for (var value of values){
-        total  += value;   
-    }
-    return total;
+function isSupported(os) {
+    return Math.random() >= 0.5;
 }
-
-
-//Doing some calculation
-
-function calculate (...values){
- var total=0;
-    for (var value of values){
-        total  += value;
-    }
-    return total;
-}
-
-console.log(calculate(1,2,3,4,5,6));
-
-
-//Doing calcalation with some options
-
-function calculate1 (action,...values){
- var total=0;
-    for (var value of values){
-        switch (action){
-            case 'add' :
-                total  += value;
-                break;
-            case 'substract':
-                total  -= value;
-                break;
-        }
-    }
-    return total;
-}
-
-console.log(calculate1('add', 1,2,3,4,5,6));
-
-//Inserting an array inside another array
-var source = [3,4,5];
-var dest = [1,2,...source,6,7,8];
-console.log(dest);
-
-//another example to concatenate two arrays (ES5 sintax)
-var list = [1,2,3];
-var toAdd = [4,5,6];
-Array.prototype.push.apply(list,toAdd);
-console.log(list);
-
-//Concatenate ES6 sintax
-var list = [1,2,3];
-var toAdd = [4,5,6];
-list.push(...toAdd);
-console.log(list);
