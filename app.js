d256dc3638d8d83d1fb61f0364e883d5130e1e74
rafaeljgomezg <rgomezshares@hotmail.com>
2016-09-02 21:50:59 -0400

    Chapter 02_02: setting default  parameters values

--- app.js
+++ app.js
@@ -1,2 +1,11 @@
-var todo = 'Pick up drycleaning';
-var name = 'Jess';
+var container = document.getElementById('container');
+function countdown(initial, final, interval) {
+    if (final === void 0) { final = 0; }
+    if (interval === void 0) { interval = 1; }
+    var current = initial;
+    while (current > final) {
+        container.innerHTML = current;
+        current -= interval;
+    }
+}
+countdown(10, 5, 8);
--- app.ts
+++ app.ts
@@ -1,2 +1,14 @@
-let todo:string = 'Pick up drycleaning';
-let name:string='Jess';
+var container = document.getElementById('container');
+
+function countdown(initial, final = 0, interval = 1) {
+
+    var current = initial;
+
+    while (current > final) {
+        container.innerHTML = current;
+        current -= interval;
+    }
+
+}
+
+countdown(10,5,8);
